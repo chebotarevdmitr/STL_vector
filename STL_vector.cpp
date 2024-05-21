ecd56f93bc93ccd417f2e4891aa62ac7a872f2d7
DIM <4ebotarov_dim@inbox.ru>
2024-05-21 21:12:25 +0300

    объявление вектора

--- STL_vector.cpp
+++ STL_vector.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main() {
+	vector<int> numbers;//объявление vectra  "numbers"
+	numbers.push_back(0);//вставление элемента с конца
 
+	for (int i = 1; i <= 10; i++) {
+		numbers.push_back(i);
+	}
 
 	cin.get();
 }
