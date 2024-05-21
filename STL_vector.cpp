7372d2a7ac406333faa3517dc665c21daf878267
DIM <4ebotarov_dim@inbox.ru>
2024-05-21 22:12:02 +0300

    begin

--- STL_vector.cpp
+++ STL_vector.cpp
@@ -10,11 +10,7 @@ int main() {
 		numbers.push_back(i);
 	}
 
-	//способы распечатки при помощи итератора ;
-	for (auto it = numbers.cbegin(); it != numbers.cend(); it++) {
-		*it = 20; // cообщение об ошибке (const не допустимо)
-		cout << *it << endl; 
-	}
+	
 
 
 	cin.get();
