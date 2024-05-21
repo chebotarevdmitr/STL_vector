6ac990f6780581c38ee46d39de83d4877d8f0ea7
DIM <4ebotarov_dim@inbox.ru>
2024-05-21 22:00:10 +0300

    каждый элемент 20

--- STL_vector.cpp
+++ STL_vector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	vector<int> numbers;//объявление vectra  "numbers"
-	numbers.push_back(0);//вставление элемента с конца
+	//numbers.push_back(0);//вставление элемента с конца
 
 	for (int i = 1; i <= 10; i++) {
 		numbers.push_back(i);
@@ -12,9 +12,8 @@ int main() {
 
 	//способы распечатки при помощи итератора ;
 	for (auto it = numbers.begin(); it != numbers.end(); it++) {
-		cout << *it << endl; //значение элемента
-		cout << &it << endl; //адрес итератора
-		cout << &(*it) << endl;//aдрес элемента
+		*it = 20;
+		cout << *it << endl; 
 	}
 
 
