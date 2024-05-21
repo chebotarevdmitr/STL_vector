2b0d3c8d4d3acbf820c1c01e77c38ce9686278ea
DIM <4ebotarov_dim@inbox.ru>
2024-05-21 21:51:35 +0300

    вывод 6

--- STL_vector.cpp
+++ STL_vector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	vector<int> numbers;//объявление vectra  "numbers"
-	numbers.push_back(0);//вставление элемента с конца
+	// numbers.push_back(0);//вставление элемента с конца
 
 	for (int i = 1; i <= 10; i++) {
 		numbers.push_back(i);
